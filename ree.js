75d0e63c22064e75d30d06c6653126a525c5e27d
liuy1994 <liuy1994g@gmail.com>
2020-04-09 10:14:29 +0800

    提交

---
+++ ree.js
@@ -0,0 +1,15 @@
+const fn = list => {
+  let node = null
+  let queue = []
+  if(list.length === 0) {
+    return node
+  } else {
+    
+    node = {
+      value: list[0],
+      left: null,
+      right: null
+    }
+  }
+
+}
