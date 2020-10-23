ef53c14f1c66be1ba84a57ffa6b6fe700f211a40
liuyong <liuyong@evision.ai>
2020-10-23 17:59:49 +0800

    541

--- 475.供暖器.js
+++ 475.供暖器.js
@@ -11,7 +11,15 @@
  * @return {number}
  */
 var findRadius = function(houses, heaters) {
-
+    let r = 0
+    if (heaters.length === 1) {
+        return Math.max(heaters[0] - 1, houses.length - heaters[0])
+    }
+    for (let i=0;i<heaters.length - 1;i++) {
+        let base = heaters[i+1] - heaters[i] - 1
+        r = Math.max(r, Math.ceil(base/2))
+    }
+    return r
 };
 // @lc code=end
 
---
+++ 541.反转字符串-ii.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=541 lang=javascript
+ *
+ * [541] 反转字符串 II
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    let str = ''
+    for (let i=0;i<s.length;i=i+2*k) {
+        if (s.length-i <k) {
+            str += s.slice(i, k).split('').reverse().join('')
+        } else {
+            str += s.slice(i, k).split('').reverse().join('')
+            str += s.slice(i+k, i+2*k)
+        }
+    }
+    console.log(str)
+    return str
+};
+// @lc code=end
+
+reverseStr('abcdefg', 2)
