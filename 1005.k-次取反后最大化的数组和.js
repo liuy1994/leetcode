b5249638d43ce4f4cb34a43b6aa2df954b517dbe
liuyong <liuyong@evision.ai>
2020-11-09 11:50:49 +0800

    1089

--- 1005.k-次取反后最大化的数组和.js
+++ 1005.k-次取反后最大化的数组和.js
@@ -13,15 +13,28 @@
 var largestSumAfterKNegations = function(A, K) {
     A.sort((a,b) => a-b)
     let arr = A.splice(0,K)
-    if(K%2 ===0) {
-        arr = arr.map(Math.abs)
-    }
-    if(arr.every(v => v >= 0)) {
-        arr[0] = K%2 === 0 ? arr[0] : 0 - arr[0]
+    if (arr.some(v => v === 0)) {
+        arr = arr.map(v => Math.abs(v))
+    } else if (K%2=== arr.filter(v => v < 0).length%2) {
+        arr = arr.map(v => Math.abs(v))
     } else {
-        arr = arr.map(Math.abs)
+        if (arr.every(v => v>0)) {
+            arr[0] = 0 - arr[0]
+        } else  if (arr.every(v => v<0)) {
+            arr = arr.map(v => Math.abs(v))
+            arr[arr.length-1] = 0 - arr[arr.length-1]
+        } else {
+            let idx = arr.indexOf(arr.find(v => v>0))
+            console.log(idx)
+            arr = arr.map(v => Math.abs(v))
+            if (arr[idx-1] > arr[idx]) {
+                arr[idx] = 0-arr[idx]
+            } else {
+                arr[idx-1] = 0-arr[idx-1]
+            }
+        }
     }
     return [...arr, ...A].reduce((a,b) => a+b,0)
 };
 // @lc code=end
-console.log(largestSumAfterKNegations([4,2,3], 1))
+console.log(largestSumAfterKNegations([5,6,9,-3,3], 2))
