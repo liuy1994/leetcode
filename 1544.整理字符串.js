be88704c6b05e428a26389bacf8ad18af36fba95
liuyong <liuyong@evision.ai>
2020-11-10 13:59:31 +0800

    1566

--- 1544.整理字符串.js
+++ 1544.整理字符串.js
@@ -10,7 +10,16 @@
  * @return {string}
  */
 var makeGood = function(s) {
-
+    s = s.split('')
+    for (let i=0;i<s.length-1 && s[i+1];) {
+        if (s[i+1] !== s[i] && s[i+1].toLowerCase() === s[i].toLowerCase()) {
+            s.splice(i, 2)
+            i = i-2 < 0 ? 0 : i-2
+        } else {
+            i++
+        }
+    }
+    return s.join('')
 };
 // @lc code=end
-
+console.log(makeGood('abBAcC'))
