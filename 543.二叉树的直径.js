af8814d2e045eeb1be1338e2cca54ed47f35cd2c
liuy1994 <liuy1994g@gmail.com>
2020-12-16 15:43:23 +0800

    733

--- 543.二叉树的直径.js
+++ 543.二叉树的直径.js
@@ -17,7 +17,17 @@
  * @return {number}
  */
 var diameterOfBinaryTree = function(root) {
-    
+    let arr = []
+    if (!root) return 0
+    var getDepth = node => {
+        if (!node) return 0
+        let l = getDepth(node.left), r = getDepth(node.right)
+        arr.push(l + r)
+        return 1 + Math.max(l, r)
+    }
+    arr.push(getDepth(root.left) + getDepth(root.right))
+    return arr.sort((a,b) => b-a)[0]
 };
+
 // @lc code=end
 
