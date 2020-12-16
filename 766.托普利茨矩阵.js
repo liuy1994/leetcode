19d0b64a4ab64f3e61931f79b9887dc34e7a77de
liuy1994 <liuy1994g@gmail.com>
2020-12-16 16:27:26 +0800

    766

---
+++ 766.托普利茨矩阵.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=766 lang=javascript
+ *
+ * [766] 托普利茨矩阵
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    let flag = false
+    for (let i=0;i<matrix.length;i++) {
+        
+    }
+
+    return flag
+};
+// @lc code=end
+
