4e2a506f8cc701be8d9d519ef1a5f0e819cadb2e
liuy1994 <liuy1994g@gmail.com>
2020-12-17 16:03:49 +0800

    883

--- 766.托普利茨矩阵.js
+++ 766.托普利茨矩阵.js
@@ -10,7 +10,13 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-
+    let flag = true
+    for (let i=0;i<matrix.length-1 && flag;i++) {
+        let p = matrix[i].slice(0, -1)
+        let q = matrix[i+1].slice(1)
+        flag = q.join('-') === p.join('-')
+    }
+    return flag
 };
 // @lc code=end
 
--- 812.最大三角形面积.js
+++ 812.最大三角形面积.js
@@ -10,7 +10,8 @@
  * @return {number}
  */
 var largestTriangleArea = function(points) {
-
+    // abc逆时针
+    return Math.abs((1/2)*(a[0]*b[1]+b[0]*c[1]+c[0]*a[1]-a[0]*c[1]-b[0]*a[1]-c[0]*b[1]))
 };
 // @lc code=end
-
+console.log(largestTriangleArea([[0,0],[0,1],[1,0],[0,2],[2,0]]))
