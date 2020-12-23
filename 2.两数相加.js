0ac5bbc7831b1c4dc7199fe0acac7f3d00385493
liuy1994 <liuy1994g@gmail.com>
2020-12-23 16:28:28 +0800

    2

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -18,17 +18,42 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let res = {}
-    let rest = 0
-    let current = res
-    while(!l1 || !l2 || !rest) {
-        let sum = l1.val + l1.val
-        current['val'] = (sum+rest)%10
-        current = current.next
-        rest = Math.floor((sum+rest)/10)
-        l1 = l1 && l1.next
-        l2 = l2 && l2.next
+    let l = [], r= []
+    while(l1) {
+        l.push(l1.val)
+        l1 = l1.next
     }
-    return res
+    while(l2) {
+        r.push(l2.val)
+
+        l2 = l2.next
+    }
+    let len = Math.max(l.length,r.length)
+    l = l.fill(0, l.length, len)
+    r = r.fill(0, r.length, len)
+    let sum = []
+    let rest = 0
+    for (let i=0;i<len;i++) {
+        let a = (l[i] || 0) + (r[i] || 0)
+        sum.push((a+rest)%10)
+        rest = Math.floor((a+rest)/10)
+
+    }
+    if (rest) sum.push(rest)
+    
+    let deal = list => {
+        if (list.length) {
+            return {
+                val: list.shift(),
+                next: deal(list)
+            }
+        } else {
+            return null
+        }
+    }
+    return {
+        val: sum.shift(),
+        next: deal(sum)
+    }
 };
 // @lc code=end
--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -10,7 +10,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-
+    let res = 0
+    for (let i=0;i<s.length-1;i++) {
+        for (let j=i+1;j<s.length;j++) {
+            if (s.slice(i,j).indexOf(s[j]) === -1) {
+                res = Math.max(res,j-i+1)
+            }
+        }
+    }
+    return res
 };
 // @lc code=end
 
+console.log(lengthOfLongestSubstring('pwwkew'))
