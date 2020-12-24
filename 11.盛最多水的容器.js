c8ad31ffd65cb34ce57e784cd7a4461429e7ef0d
liuy1994 <liuy1994g@gmail.com>
2020-12-24 16:38:45 +0800

    12

--- 11.盛最多水的容器.js
+++ 11.盛最多水的容器.js
@@ -10,7 +10,15 @@
  * @return {number}
  */
 var maxArea = function(height) {
-
+    let res = 0
+    for (let i=0;i<height.length-1;i++) {
+        for (let j=i+1;j<height.length;j++) {
+            let r = (j-i)*Math.min(height[i],height[j])
+            res = r > res ? r : res
+        }
+    }
+    return res
 };
 // @lc code=end
 
+// console.log(maxArea([1]))
--- 15.三数之和.js
+++ 15.三数之和.js
@@ -10,51 +10,41 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums = nums.sort((a,b) => a-b)
     let res = []
-    let a = []
-    let b = []
-    let c = []
-    nums.forEach(v => {
-        if (v === 0) b.push(v)
-        if (v > 0) c.push(v)
-        if (v < 0) a.push(v)
-    })
-    if (!a.length && b.length >= 3 && !c.length) res.push([0,0,0])
-    if (b.length) {
-        [...new Set(a)].forEach(v => {
-            if (c.includes(Math.abs(v))) {
-                res.push([v, 0, Math.abs(v)])
+    nums.sort((a,b) => a-b)
+    
+    for (let i=0;i<nums.length-2 && nums[i] <=0;i++) {
+        for (let j=nums.length-1;j>1 && nums[j] >=0;j--) {
+            let rest = 0-nums[i]-nums[j]
+            let key = [nums[i],rest, nums[j]].sort((a,b) => a-b).join(',')
+            
+            let index = nums.indexOf(rest)
+            let lastIndex = nums.lastIndexOf(rest)
+            if ((nums[i] === nums[j])) {
+                if (nums.lastIndexOf(0)-nums.indexOf(0) > 1) {
+                    if (res.includes(key)) {
+                        continue
+                    }
+                    res.push('0,0,0')
+                } else {
+                    continue
+                }
             }
-        })
-    }
-
-    let aObj = {}
-    let cObj ={}
-
-    for (let i=0;i<a.length-1;i++) {
-        for (let j=i+1;j<a.length;j++) {
-            aObj[`${a[i]}/${a[j]}`] = a[i] + a[j]
+            if (res.includes(key)) {
+                continue
+            }
+            if (index > -1) {
+                if ((index !== lastIndex)) {
+                    res.push(key)
+                } else if (index !== i && index !== j) {
+                    res.push(key)
+                }
+            }
         }
     }
-    for (let i=0;i<c.length-1;i++) {
-        for (let j=i+1;j<c.length;j++) {
-            cObj[`${c[i]}/${c[j]}`] = c[i] + c[j]
-        }
-    }
-    console.log(aObj, cObj)
-    for (let key in aObj) {
-        if (c.includes(Math.abs(aObj[key]))) {
-            res.push([parseInt(key.split('/')[0]) , parseInt(key.split('/')[1]), Math.abs(aObj[key])])
-        }
-    }
-    for (let key in cObj) {
-        if (a.includes(0 - aObj[key])) {
-            res.push([parseInt(key.split('/')[0]) , parseInt(key.split('/')[1]), 0 - aObj[key]])
-        }
-    }
-    return res
+    return res.map(v => v.split(','))
 };
 // @lc code=end
 
-console.log(threeSum([1,1,-2]))
+// console.log(threeSum([34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]))
+// console.log(threeSum([0,0,0,0]))
