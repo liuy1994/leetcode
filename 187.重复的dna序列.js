0b1bf1bd44468d8913807c0cfa9fad45bbfedd57
liuyong <liuyong@evision.ai>
2021-03-04 21:01:20 +0800

    187

---
+++ 187.重复的dna序列.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=187 lang=javascript
+ *
+ * [187] 重复的DNA序列
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function(s) {
+    let res = {}
+    while(s.length >= 10) {
+        let str = s.slice(0, 10)
+        res[str] = res[str] ? res[str]+1 : 1
+        s = s.slice(1)
+    }
+    // console.log(res)
+    return Object.entries(res).filter(v => v[1] > 1).map(v => v[0])
+};
+// @lc code=end
+
+// console.log(findRepeatedDnaSequences('AAAAAAAAAAA'))
