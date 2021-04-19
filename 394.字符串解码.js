a2da3bf762c453e50ff46c5ef993cc62500421df
liuy1994g <liuy1994g@gmail.com>
2021-04-19 17:28:09 +0800

    397

---
+++ 394.字符串解码.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=394 lang=javascript
+ *
+ * [394] 字符串解码
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+
+};
+// @lc code=end
+
---
+++ 400.第-n-位数字.js
@@ -0,0 +1,16 @@
+
+/*
+ * @lc app=leetcode.cn id=400 lang=javascript
+ *
+ * [400] 第 N 位数字
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+
+};
+// @lc code=end
