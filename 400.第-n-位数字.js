b4551cda906624a9fd2c17fac6ced79338dcd99f
liuy1994g <liuy1994g@gmail.com>
2021-04-26 16:45:00 +0800

    423

--- 400.第-n-位数字.js
+++ 400.第-n-位数字.js
@@ -11,6 +11,25 @@
  * @return {number}
  */
 var findNthDigit = function(n) {
-
+    // let res = ''
+    // let base = 1
+    // while(res.length < n) {
+    //     res = res + (base++).toString()
+    // }
+    // return res[n-1]
+    let count = 0
+    let current = 0
+    while(count < n) {
+        let temp = count + (current+1).toString().length
+        if (temp < n) {
+            count = temp
+            current++
+        } else {
+            break
+        }
+    }
+    let last = (current+1).toString()
+    return parseInt(last[n-count-1])
 };
 // @lc code=end
+// console.log(findNthDigit(13))
