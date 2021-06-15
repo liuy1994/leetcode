5bc929947eeaeb95b0e6c3d3fc394217b1c2d452
刘勇 <liam.liu@wayz.ai>
2021-06-15 17:06:29 +0800

    316

--- 316.去除重复字母.js
+++ 316.去除重复字母.js
@@ -10,17 +10,6 @@
  * @return {string}
  */
 var removeDuplicateLetters = function(s) {
-    s = s.split('')
-    for (let i=0;i<s.length;i++) {
-        if (s.lastIndexOf(s[i]) !== i) {
-            if (s.slice(i).some(v => v < s[i])) {
-                s[i] = '_'
-            }
-        }
-    }
-    let res = ''
 
-    return s
 };
 // @lc code=end
-console.log(removeDuplicateLetters('cbacdcbc'))
