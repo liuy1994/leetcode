bd07f8b0bb19b24a570ff5b15de3aa95b8f52682
liuy1994 <liuy1994@outlook.com>
2021-07-14 17:02:41 +0800

    130

---
+++ 130.被围绕的区域.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=130 lang=javascript
+ *
+ * [130] 被围绕的区域
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solve = function(board) {
+    let m = board.length, n = board[0].length
+    
+};
+// @lc code=end
+
