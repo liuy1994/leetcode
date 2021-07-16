475436e3e48cdedd0b647c18925e1ff95083b633
liuy1994 <liuy1994@outlook.com>
2021-07-16 10:32:46 +0800

    init 525

--- 525.连续数组.js
+++ 525.连续数组.js
@@ -10,15 +10,7 @@
  * @return {number}
  */
 var findMaxLength = function(nums) {
-    let max = 0
-    for (let len = nums.length;len>=2;len--) {
-        for (let i=0;i<=nums.length-len;i++) {
-            if (nums.slice(i, i+len).filter(v => v === 1).length === len/2) {
-                return len
-            }
-        }
-    }
-    return max
+    
 };
 // @lc code=end
 
