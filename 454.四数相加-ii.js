a9e8811974387aa3bb97987e7e40656e6137728f
liuy1994 <liuy1994@outlook.com>
2021-07-19 15:21:14 +0800

    454

--- 454.四数相加-ii.js
+++ 454.四数相加-ii.js
@@ -14,15 +14,17 @@
  */
 var fourSumCount = function(nums1, nums2, nums3, nums4) {
     let len = nums4.length
-    let res = 0
-    for (let a=0;a<len;a++) {
-        for (let b=0;b<len;b++) {
-            for (let c=0;c<len;c++) {
-                for (let d=0;d<len;d++) {
-                    if (nums1[a] + nums2[b] + nums3[c] + nums4[d] === 0) {
-                        res++
-                    }
-                }
+    let a = {}, res = 0
+    for (i of nums1) {
+        for (let j of nums2) {
+            a[i+j] = a[i+j] ? (a[i+j] + 1) : 1
+        }
+    }
+    for (i of nums3) {
+        for (let j of nums4) {
+            let sum = 0-i-j
+            if (a[sum]) {
+                res += a[sum]
             }
         }
     }
