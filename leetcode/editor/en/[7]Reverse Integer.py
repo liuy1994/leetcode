6f6800471e7828f5f7e7a137eddaa7d4f082e8cf
liuy1994 <liuy1994@outlook.com>
2021-07-19 16:20:34 +0800

    'py'

---
+++ leetcode/editor/en/[7]Reverse Integer.py
@@ -0,0 +1,36 @@
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversi
+# ng x causes the value to go outside the signed 32-bit integer range [-231, 231 -
+#  1], then return 0. 
+# 
+#  Assume the environment does not allow you to store 64-bit integers (signed or
+#  unsigned). 
+# 
+#  
+#  Example 1: 
+#  Input: x = 123
+# Output: 321
+#  Example 2: 
+#  Input: x = -123
+# Output: -321
+#  Example 3: 
+#  Input: x = 120
+# Output: 21
+#  Example 4: 
+#  Input: x = 0
+# Output: 0
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  -231 <= x <= 231 - 1 
+#  
+#  Related Topics Math 
+#  👍 5183 👎 7810
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
