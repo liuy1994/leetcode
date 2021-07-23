59112d6c15d35334334227bf774ae5e9056c66a3
liuy1994 <liuy1994@outlook.com>
2021-07-23 09:52:01 +0800

    'py58'

---
+++ leetcode/editor/en/[58]Length of Last Word.py
@@ -0,0 +1,29 @@
+# Given a string s consists of some words separated by spaces, return the length
+#  of the last word in the string. If the last word does not exist, return 0. 
+# 
+#  A word is a maximal substring consisting of non-space characters only. 
+# 
+#  
+#  Example 1: 
+#  Input: s = "Hello World"
+# Output: 5
+#  Example 2: 
+#  Input: s = " "
+# Output: 0
+#  
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 104 
+#  s consists of only English letters and spaces ' '. 
+#  
+#  Related Topics String 
+#  👍 1257 👎 3404
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        
+# leetcode submit region end(Prohibit modification and deletion)
