e58306d5d03cb50d03d7ed31432101cd64fe7a48
liuy1994 <liuy1994@outlook.com>
2022-08-18 15:21:26 +0800

    11

--- 11.盛最多水的容器.js
+++ 11.盛最多水的容器.js
@@ -10,32 +10,44 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let res = 0
-    for (let i = 0; i< height.length - 1;i++) {
-        let base = null
-        for (let j = height.length - 1;j>i;j--) {
-            if (base) {
-                console.log(`arr: ${height.slice(i+1, j+1).toString()}`)
-                if (Math.max(...height.slice(i+1, j)) < height[base]) {
-                    break
-                }
-                if (height[j] <= height[base]) {
-                    continue
-                }
-            }
+    // let res = 0
+    // for (let i = 0; i< height.length - 1;i++) {
+    //     let base = null
+    //     for (let j = height.length - 1;j>i;j--) {
+    //         if (base) {
+    //             if (Math.max(...height.slice(i+1, j+1)) < height[base]) {
+    //                 break
+    //             }
+    //             if (height[j] <= height[base]) {
+    //                 continue
+    //             }
+    //         }
             
-            console.log(height[i], height[j])
-            const current = (j - i) * Math.min(height[i], height[j])
-            if (current > res) {
-                res = current
-                base = j
-            }
-            console.log(`res: ${res}, base: ${height[base]}`)
+    //         const current = (j - i) * Math.min(height[i], height[j])
+    //         if (current > res) {
+                
+    //             res = current
+    //             base = j
+    //         }
+    //     }
+    // }
+    // return res
+    let l = 0, r = height.length - 1
+    let res = Math.min(height[l], height[r]) * (r - l)
+    while(l < r) {
+        if (height[r] < height[l]) {
+            r--
+        } else {
+            l++
+        }
+
+        const sum = Math.min(height[l], height[r]) * (r - l)
+        if (sum >= res) {
+            res = sum
         }
     }
-    // console.log(res)
     return res
 };
 // @lc code=end
 
-console.log(maxArea([1,1000,1000,6,2,5,4,8,3,7]))
+// console.log(maxArea([1,1000,1000,6,2,5,4,8,3,7]))
