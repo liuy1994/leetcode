1a900b0b804d55c97ba47af09386998591500096
liuy1994 <liuy1994@outlook.com>
2022-10-10 16:15:45 +0800

    25

--- 24.两两交换链表中的节点.js
+++ 24.两两交换链表中的节点.js
@@ -19,20 +19,9 @@
 var swapPairs = function(head) {
     if (!head || !head.next) return head
     let next = head.next
-    // next.next = swapPairs(next.next)
     head.next = swapPairs(next.next)
     next.next = head
-
-
     return next
-    // let pos = head
-    // while(pos && pos.next) {
-    //     let temp = pos.next.val
-    //     pos.next.val = pos.val
-    //     pos.val = temp
-    //     pos = pos.next.next
-    // }
-    // return head
 };
 // @lc code=end
 
