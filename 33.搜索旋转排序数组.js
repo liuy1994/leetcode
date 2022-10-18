f5dd914297c14fe59fc2f24c8c1b96801487f05e
liuy1994 <liuy1994@outlook.com>
2022-10-18 10:23:40 +0800

    33

--- 33.搜索旋转排序数组.js
+++ 33.搜索旋转排序数组.js
@@ -14,11 +14,12 @@ var search = function(nums, target) {
     // return nums.indexOf(target)
     let l = 0, r = nums.length - 1, mid = Math.floor((l+r)/2)
 
-   while(l < r) {
+   while(l <= r) {
+    // console.log(nums[l], nums[mid], nums[r])
     if (nums[mid] === target) {
         return mid
     }
-    if (nums[mid] > nums[l]) {
+    if (nums[mid] >= nums[l]) {
         // 左边有序
         if (nums[l] <= target && nums[mid] > target) {
             // 在左边
@@ -43,4 +44,4 @@ var search = function(nums, target) {
 };
 // @lc code=end
 
-console.log(search([4,5,6,7,0,1,2], 0))
+// console.log(search([1], 0))
