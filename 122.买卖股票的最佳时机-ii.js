d9466d7d6499347349ca176940d1ea9aa0e58572
liuy1994 <liuy1994@outlook.com>
2023-09-21 14:48:42 +0800

    45

--- 122.买卖股票的最佳时机-ii.js
+++ 122.买卖股票的最佳时机-ii.js
@@ -10,24 +10,35 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let arr = [...prices]
-    // console.log(prices.sort((a,b) => a-b))
-    if (prices.join('-') === arr.sort((a,b) => a-b).join('-')) {
-        return prices[prices.length-1]  - prices[0]
-    }
-    if (prices.join('-') === arr.sort((a,b) => b-a).join('-')) {
-        return 0
-    }
     let res = 0
-    let base = 0
-    for(let i=0;i<prices.length-1;i++) {
-        if (prices[i+1] > prices[i]) {
-            res += prices[i+1] - prices[i]
-            base = i+1
+    const len = prices.length
+    let p=0,q=1
+    while(p < len - 1) {
+        if (prices[p] < prices[q]) {
+            res += prices[q] - prices[p]
         }
+        p++
+        q++
     }
     return res
+    // let arr = [...prices]
+    // // console.log(prices.sort((a,b) => a-b))
+    // if (prices.join('-') === arr.sort((a,b) => a-b).join('-')) {
+    //     return prices[prices.length-1]  - prices[0]
+    // }
+    // if (prices.join('-') === arr.sort((a,b) => b-a).join('-')) {
+    //     return 0
+    // }
+    // let res = 0
+    // let base = 0
+    // for(let i=0;i<prices.length-1;i++) {
+    //     if (prices[i+1] > prices[i]) {
+    //         res += prices[i+1] - prices[i]
+    //         base = i+1
+    //     }
+    // }
+    // return res
 };
 // @lc code=end
 
-console.log(maxProfit([7,1,5,3,6,4]))
+// console.log(maxProfit([7,1,5,3,6,4]))
--- 55.跳跃游戏.js
+++ 55.跳跃游戏.js
@@ -10,24 +10,31 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    if (nums.filter(v=> !!v).length === 0) return true
-    if (nums[0] === 0) return false
-    if (nums.length === 1) return true
-    let i=nums.length-2
-    for (;i>=1;i--) {
-        if (nums[i] !== 0) {
-            continue
-        } else {
-            if (nums.slice(0, i).find((v,j) => v-(i-j) >0)) {
-                continue
-            } else {
-                return false
-            }
-        }
+    // if (nums.filter(v=> !!v).length === 0) return true
+    // if (nums[0] === 0) return false
+    // if (nums.length === 1) return true
+    // let i=nums.length-2
+    // for (;i>=1;i--) {
+    //     if (nums[i] !== 0) {
+    //         continue
+    //     } else {
+    //         if (nums.slice(0, i).find((v,j) => v-(i-j) >0)) {
+    //             continue
+    //         } else {
+    //             return false
+    //         }
+    //     }
+    // }
+    // return i === 0
+    const len = nums.length
+
+    let max = 0
+    for (let i=0;i<len-1;i++) {
+        max = Math.max(max, i + nums[i])
+        if (max <= i) return false
     }
-    return i === 0
+    return max >= len - 1
+
 
 };
 // @lc code=end
-
-// console.log(canJump([1]))
